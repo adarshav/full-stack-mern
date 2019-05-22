1522c7acb2d9662d53d22303b5b4efcd31a152ec
adarsha <adarshav957@gmail.com>
2019-05-22 15:24:26 +0530

    Switch stateents and ternary operators

--- udemy-javascript/1-JS-Basics/script.js
+++ udemy-javascript/1-JS-Basics/script.js
@@ -61,10 +61,114 @@ Mark and john are trying to compare their BMI(Body Mass Index), which is calcula
 // console.log("Is Mark's BMI higher than John's" + ' ' + isHighest);
 
 //if- else
-var name = 'adarsha';
-var civilStatus = 'single';
-if(civilStatus === 'married') {
-    console.log(name + ' ' + 'is married');
-} else { 
-    console.log(name + ' ' + 'will hopefully marry soon');
+// var name = 'adarsha';
+// var civilStatus = 'single';
+// if(civilStatus === 'married') {
+//     console.log(name + ' ' + 'is married');
+// } else { 
+//     console.log(name + ' ' + 'will hopefully marry soon');
+// }
+
+//Boolean Logic
+/*
+    AND operator holds true if both values are true
+    OR operator holds true if one of the values holds true
+    NOT negates thee value
+ */
+// var age = 23;
+// var name = 'adarsha';
+// if(age <= 13) {
+//     console.log(name + ' ' + 'is a boy');
+// } else if(age >= 13 && age <= 20) {
+//     console.log(name + ' is a teenager');
+// } else if(age >=20 && age <= 30) {
+//     console.log(name + ' is a young man');
+// } else{
+//     console.log(name + ' is a Man');
+// }
+
+//********************* */
+//Ternary operator andSwith statements
+// var age = 15;
+// var name = 'adarsha';
+// (age >= 18) ? console.log(name + ' can vote for the country') : console.log(name + ' has to wait for some more years');
+// vote = (age >= 18) ? 'vote':'cannot vote';
+// console.log( name + ' ' + vote);
+
+//Swith statements
+//it is another way addressing the if-else-if statements
+// var name = 'adarsha';
+// var job = 'actor';
+// switch(job) {
+//     case 'teacher':
+//     case 'instructor':
+//         console.log(name + ' teaches about the untold story of indians who fought for the sake of country selflessely');
+//         break;
+//     case 'employee':
+//         console.log(name + ' works in a prestigious company with more salary');
+//         break;
+//     case 'speaker':
+//         console.log(name + 'speaks about the current issues faced by india');
+//         break;
+//     default:
+//         console.log(name + ' does something else');
+// }
+//there is no rule that case condition should be a variable or another, it can be an condition too like the below code
+// var age = 30;
+// switch(true) {
+//     case (age < 18):
+//         console.log(name + ' is a boy');
+//         break;
+//     case (age >= 18 && age <= 25):
+//         console.log(name + ' is teenager');
+//         break;
+//     case (age >=  25):
+//         console.log(name + ' is a Man');
+//         break;
+//     default:
+//         console.log(name + 'He is something else');
+// }
+
+//Truthy and falsy values
+//falsy values are Undefined, null, NaN, 0, ''
+//Other than falsy values are Truthy values
+// var height;
+// height = 23;
+// if(height) {
+//     console.log('the variable has been defined');
+// } else {
+//     console.log('the variable has NOT been defined');
+// }
+
+//Equality operator
+//== operator does type coersion whereas === operator strictly checks and type coercion is not possible here 
+// if(height === '23') {
+// } else {
+//     console.log('something else');
+// }
+
+//********************************************************** */
+//CODING CHALLENGE 2
+/*
+    john and Mike both play basketball in different teams, in the latest 3 games, john's team scored 89, 120, 103 points while Mike's team scored 116, 94, 123 points.
+    Calculate average score for  each team
+    Decide which teams in average (highest average score) and print winner to consoleand include average score in the output and take draw into the account
+    Extra mary also plays the basketball with the points 97, 134, 105 points
+ */
+var johnGame1 = 89;
+var johnGame2 = 120;
+var johnGame3 = 103;
+var johnAverage = (johnGame1 + johnGame2 + johnGame3) / 3;
+var mikeGame1 = 116;
+var mikeGame2 = 94;
+var mikeGame3 = 123;
+var mikeAverage = (mikeGame1 + mikeGame2 + mikeGame3) / 3;
+if(johnAverage > mikeAverage) {
+    console.log('John is the winner' + 'with' + johnAverage);
+} else {
+    console.log('mike is the winner' + 'with' + mikeAverage);
+
 }
+
+
+
